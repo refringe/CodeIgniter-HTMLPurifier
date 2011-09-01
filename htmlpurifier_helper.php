c079caf4b9240125d966d23d080654ff98a9e3be
Tyler Brownell <brownelltyler@gmail.com>
2011-09-01 17:02:31 -0400

    Hello GitHub!

---
+++ htmlpurifier_helper.php
@@ -0,0 +1,65 @@
+<?php if (! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Codeigniter HTMLPurifier Helper
+ * 
+ * Purify input using the HTMLPurifier standalone class.
+ * Easily use multiple purifier configurations.
+ * 
+ * @author     Tyler Brownell
+ * @copyright  Copyright © 2011 Blue Fox Studio
+ * @license    http://bluefoxstudio.ca/license.html
+ * 
+ * @access  public
+ * @param   string or array
+ * @param   string
+ * @return  string or array
+ */
+if (! function_exists('html_purify'))
+{
+	function html_purify($dirty_html, $config = false)
+	{
+		require_once APPPATH . 'third_party/htmlpurifier-4.3.0-standalone/HTMLPurifier.standalone.php';
+		
+		if (is_array($dirty_html))
+		{
+			foreach ($dirty_html as $key => $val)
+			{
+				$clean_html[$key] = html_purify($val);
+			}
+		}
+		
+		else
+		{
+			switch ($config)
+			{
+				case 'comment':
+					$config = HTMLPurifier_Config::createDefault();
+					$config->set('Core.Encoding', 'utf-8');
+					$config->set('HTML.Doctype', 'XHTML 1.0 Strict');
+					$config->set('HTML.Allowed', 'p,a[href|title],abbr[title],acronym[title],b,strong,blockquote[cite],code,em,i,strike');
+					$config->set('AutoFormat.AutoParagraph', true);
+					$config->set('AutoFormat.Linkify', true);
+					$config->set('AutoFormat.RemoveEmpty', true);
+					break;
+					
+				case false:
+					$config = HTMLPurifier_Config::createDefault();
+					$config->set('Core.Encoding', 'utf-8');
+					$config->set('HTML.Doctype', 'XHTML 1.0 Strict');
+					break;
+					
+				default:
+					show_error('The HTMLPurifier configuration labeled "' . htmlentities($config, ENT_QUOTES, 'UTF-8') . '" could not be found.');
+			}
+			
+			$purifier = new HTMLPurifier($config);
+			$clean_html = $purifier->purify($dirty_html);
+		}
+		
+		return $clean_html;
+	}
+}
+
+/* End of htmlpurifier_helper.php */
+/* Location: ./application/helpers/htmlpurifier_helper.php */
